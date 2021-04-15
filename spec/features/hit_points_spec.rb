004785ddb7c70031319751af4cd17f501408f0ee
Matt <mattdawson18@gmail.com>
2021-04-15 15:08:02 +0100

    helper methods

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,8 @@
+# require_relative 'web_helpers'
+
 feature 'Enter player hit points' do
   scenario 'player 2 has hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Matt'
-    fill_in :player_2_name, with: 'Toby'
-    click_button 'Submit'
-
+    sign_in_and_play
     # save_and_open_page
     
     expect(page).to have_content 'HP:50'
--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -1,16 +1,11 @@
-# describe "name", type: :feature do
-  
+require_relative 'web_helpers'
+
   feature 'Enter player names' do
     scenario 'submitting player names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Matt'
-      fill_in :player_2_name, with: 'Toby'
-      click_button 'Submit'
-
-      save_and_open_page
+      sign_in_and_play
+      # save_and_open_page
       
       expect(page).to have_content 'Matt vs. Toby'
     end
   end
   
-# end
